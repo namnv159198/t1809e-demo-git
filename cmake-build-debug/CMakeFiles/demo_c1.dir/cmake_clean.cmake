2df7e2c2c050002a71b17f95e248523a74fd5df7
namnvth1809002 <namnvth1809002@fpt.edu.vn>
2018-09-24 19:15:18 +0700

    first demo c

---
+++ cmake-build-debug/CMakeFiles/demo_c1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo_c1.dir/main.c.obj"
+  "demo_c1.pdb"
+  "demo_c1.exe"
+  "demo_c1.exe.manifest"
+  "libdemo_c1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/demo_c1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/demo_c1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for demo_c1.
+# This may be replaced when dependencies are built.
